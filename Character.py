345d060d0c01b85fce10633164f677e96e2938da
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-18 17:47:14 +0100

    Creating sub classes hero and enemy

--- Character.py
+++ Character.py
@@ -11,3 +11,15 @@ class Character:
 def attack(self, target) -> None:
     target.health -= self.weapon.damage
     target.health = max(target.health, 0)
+
+
+# Creating a sub class Hero 
+class Hero(Character): 
+    def __init__(self, name:str, health:int) -> None:
+        super().__init__(name=name, health=health)
+
+
+# Creating a sub class Enemy
+class Enemy(Character): 
+    def __init__(self, name:str, health:int) -> None:
+        super().__init__(name=name, health=health)
