ff56e8559fe19f2b9adcb13770cdfa15cbddec7b
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-18 17:31:37 +0100

    Create Character.py

---
+++ Character.py
@@ -0,0 +1,11 @@
+class Character: 
+
+    def __init__(self, name: str, health: int, damage: int):
+        self.name = name
+        self.health = health
+        self.health_max = health
+        self.damage = damage 
+
+def attack(self, target) -> None:
+    target.hp -= self.damage 
+    target.hp = max(target.hp, 0)
